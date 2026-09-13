81513c9d987386b0229d9622f002858908a03846
MadComputationalPhys2003 <sashaknyazev2016@gmail.com>
2026-09-13 18:40:21 +0500

    some changes

--- Shredinger_Equation/Shredinger_Equation/Shredinger_Equation.cpp
+++ Shredinger_Equation/Shredinger_Equation/Shredinger_Equation.cpp
@@ -11,20 +11,24 @@ namespace Eigen {
 		double xmin; //Value of x at the left boundary
 		double xmax; //Value of x at the right boundary
 		double h; //Step size for the numerical method
-		std::function<double(double)> coefficient_k; //2*(potetial-energy)
+		std::function<double(double)> coefficient_k; //2*(energy-potetial)
 		std::function<double(double)> coefficient_k_diff; //Differential of 2*(energy- potential)
 		std::vector<double> x; //Vector of x values
 		std::vector<double> psi; //Vector of wavefunction values
 		std::vector<double> psi_diff; //Vector of wavefunction derivative values
 		size_t N; //Number of steps in the numerical method
-		init(double xmin, double xmax, std::function<double(double)> coefficient_k, std::function<double(double)> coefficient_k_diff, double h, parity par)
-			: xmin(xmin), xmax(xmax), coefficient_k(coefficient_k), coefficient_k_diff(coefficient_k_diff), h(h), wavefunction_parity(par)
+		init(parity par, double h,double xmin, double xmax, std::function<double(double)> coefficient_k, std::function<double(double)> coefficient_k_diff)
+			: wavefunction_parity(par),  h(h), xmin(xmin), xmax(xmax), coefficient_k(coefficient_k), coefficient_k_diff(coefficient_k_diff)
 		{
-			N =  static_cast<size_t>(std::round(std::abs(xmax) * 1.0 / (2 * h) + 1));
+			N =  static_cast<size_t>(std::round(std::abs(xmax) * 1.0 / (h) + 1));
+			x.resize(N);
+			for(size_t i=0; i<N; i++)
+			{
+				x[i] = xmin + i * h;
+			}
+			psi.resize(N);
+			psi_diff.resize(N);
 			if (wavefunction_parity == parity::Even) {
-				x.resize(N);
-				psi.resize(N);
-				psi_diff.resize(N);
 				psi0 = 1.0; //Initial value of the wavefunction at the left boundary in Even case
 				psi0_diff = 0.0; //Initial value of the derivative of the wavefunction at the left boundary in Even Case
 				psi[0] = psi0;
@@ -32,9 +36,7 @@ namespace Eigen {
 				psi[1] = bootstrap(wavefunction_parity);
 			}
 			else if (wavefunction_parity == parity::Odd) {
-				x.resize(N);
-				psi.resize(N);
-				psi_diff.resize(N);
+				
 				psi0 = 0; //Initial value of the wavefunction at the left boundary in Even case
 				psi0_diff = 1.0; //Initial value of the derivative of the wavefunction at the left boundary in Even Case
 				psi[0] = psi0;
@@ -50,12 +52,11 @@ namespace Eigen {
 		double bootstrap(parity par) {
 			double psi1;
 			if (par == parity::Even) {
-				psi1 = 1 - h * h * coefficient_k(x[0]) - (h * h) / (12)
-					* coefficient_k_diff(x[0]);
+				psi1 = 1 - (h * h / 2.0) * coefficient_k(x[0]) - (h * h * h / 6.0) * coefficient_k_diff(x[0]);
 			}
 			else
 			{
-				psi1 = h - (h * h * h) * 1.0 / (6) * coefficient_k(x[0]);
+				psi1 = 1 - (h * h / 2.0) * coefficient_k(x[0]) - (h * h * h / 6.0) * coefficient_k_diff(x[0]);
 			}
 			return psi1;
 		}
