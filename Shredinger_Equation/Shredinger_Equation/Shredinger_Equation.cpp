e8e4515db7874540dc352c6a477dabd4f8fce2dc
MadComputationalPhys2003 <sashaknyazev2016@gmail.com>
2026-09-13 19:22:17 +0500

    changes

--- Shredinger_Equation/Shredinger_Equation/Shredinger_Equation.cpp
+++ Shredinger_Equation/Shredinger_Equation/Shredinger_Equation.cpp
@@ -2,6 +2,9 @@
 #include<functional>
 #include <vector>
 #include <cmath>
+#include<cstdint>
+#include<numbers>
+constexpr double pi=std::numbers::pi;
 //Like program "Eigen" from Guld part 2 but used Numerov Method(Problems 17.1-17.4)
 namespace Eigen {
 	class init {
@@ -56,14 +59,47 @@ namespace Eigen {
 			}
 			else
 			{
-				psi1 = h-(h * h * h / 6.0) * coefficient_k_diff(x[0]);
+				psi1 = h-(h * h * h / 6.0) * coefficient_k(x[0]);
 			}
 			return psi1;
 		}
 	};
+	void numerow_method(init& ini,const double& psi_max) {
+		double alpha_1 = (1 + (ini.h * ini.h) / 12.0 * ini.coefficient_k(ini.x[0]));
+		double alpha_2 = 2*(1-(5*ini.h*ini.h/12.0*ini.coefficient_k(ini.x[1])));
+		double alpha_3 = (1 + (ini.h * ini.h) / 12.0 * ini.coefficient_k(ini.x[2]));
+		for (size_t i = 2; i < ini.N; i++) {
+			ini.psi[i] = (alpha_2 * ini.psi[i - 1] - alpha_1 * ini.psi[i - 2]) * 1.0 / (alpha_3);
+			if (ini.psi[i] > psi_max) {
+				std::cout << "Warning: Wavefunction exceeded maximum value at x = " << ini.x[i] << std::endl;
+				break;
+			}
+			alpha_1 = alpha_2;
+			alpha_2 = alpha_3;
+			alpha_3 = (1 + (ini.h * ini.h) / 12.0 * ini.coefficient_k(ini.x[i + 1]));
+		}
+	}
+
 
 
 }
+double potential_1(double x,double a,double V0) {
+	if (std::abs(x) <= a) {
+		return 0;
+	}
+	else{
+		return V0;
+	}
+}
+double func(double x) {
+	uint8_t n = 1;
+	double a = 1;
+	double V0 = 20;
+	double E = n * n * pi * pi / (8 * a * a);
+	return 2 * (E - potential_1(x, a, V0));
+}
+
+
 
 int main()
 {
