54dc35546a886302b2ce86791250608d48530043
MadComputationalPhys2003 <sashaknyazev2016@gmail.com>
2026-09-20 01:53:04 +0500

    add Lattice3D_Data, create_lattice3D and cubic lattice setter

--- FaustSpin/FaustSpin/Latice3D.h
+++ FaustSpin/FaustSpin/Latice3D.h
@@ -1,25 +1,97 @@
 #pragma once
 #include<array>
+#include<functional>
+#include<stdexcept>
+#include<tuple>
 #include<cstdint>
 #include<vector>
+
+
 
 struct Cell
 {
 	int64_t x, y, z;
 	Cell(int64_t x=0, int64_t y=0, int64_t z=0) : x(x), y(y), z(z) {}
 };
-class Latice3D{
-private:
-	uint64_t Lx, Ly, Lz; // Dimensions of the lattice
-	size_t N; // Total number of cells
-	int64_t x0, y0, z0; // Coordinates of the reference cell
-	Cell cell0; // Reference cell
-	std::vector<Cell> cells;
-public:
-	explicit Latice3D(uint64_t Lx, uint64_t Ly, uint64_t Lz,
-		int64_t x0=0, int64_t y0=0, int64_t z0=0) :
-		Lx(Lx), Ly(Ly), Lz(Lz), x0(x0), y0(y0), z0(z0), cell0(x0, y0, z0) {
-		N = static_cast<size_t>(Lx *Ly *Lz);
-		cells.resize(N);
+namespace Lattice3D {
+	class Latice3D_Data {
+	private:
+		uint64_t Lx, Ly, Lz; // Dimensions of the lattice
+		size_t N; // Total number of cells
+		std::vector<Cell> cells;// Vector to store the cells
+		Cell cell0; // Reference cell
+	public:
+		explicit Latice3D_Data(uint64_t Lx, uint64_t Ly, uint64_t Lz,
+			int64_t x0 = 0, int64_t y0 = 0, int64_t z0 = 0) :
+			Lx(Lx), Ly(Ly), Lz(Lz), cell0(x0, y0, z0) {
+			N = static_cast<size_t>(Lx * Ly * Lz);
+			cells.resize(N);
+		}
+		const Cell& get_cell(size_t index) const {
+			if (index >= N) throw std::out_of_range("Index out of range");
+			return cells[index];
+		}
+		std::vector<Cell> get_cells() const {
+			return cells;
+		}
+		void set_cells(const std::vector<Cell>& new_cells) {
+			if (new_cells.size() != N) throw std::invalid_argument("New cells vector size must match the total number of cells");
+			cells = new_cells;
+		}
+		const Cell& get_cell0() const {
+			return cell0;
+		}
+		const uint64_t& get_Lx() const { return Lx; }
+		const uint64_t& get_Ly() const { return Ly; }
+		const uint64_t& get_Lz() const { return Lz; }
+		const size_t& get_N() const { return N; }
+		std::array<int64_t, 3> get_coords(size_t idx) const {
+			if (idx >= N) throw std::out_of_range("Index out of range");
+			int64_t x=idx/(Ly*Lz);
+			int64_t r = idx % (Ly * Lz);
+			int64_t y = r / Lz;
+			int64_t z = r % Lz;
+			return {x, y, z};
+		}
+		size_t get_index(int64_t x, int64_t y, int64_t z) const {
+			if (x < 0 || x >= static_cast<int64_t>(Lx) ||
+				y < 0 || y >= static_cast<int64_t>(Ly) ||
+				z < 0 || z >= static_cast<int64_t>(Lz)) {
+				throw std::out_of_range("Coordinates out of range");
+			}
+			return static_cast<size_t>(x * Ly * Lz + y * Lz + z);
+		}
+	};
+	inline Latice3D_Data create_lattice3D(const Latice3D_Data& ld, std::function<Cell(const Latice3D_Data&, int64_t, int64_t, int64_t)> func) {
+		std::vector<Cell> new_cells(ld.get_N());
+		size_t ion_count = 0;
+		for (size_t x = 0; x < ld.get_Lx() && ion_count < ld.get_N(); x++) {
+			for (size_t y = 0; y < ld.get_Ly() && ion_count < ld.get_N(); y++) {
+				for (size_t z = 0; z < ld.get_Lz() && ion_count < ld.get_N(); z++) {
+					new_cells[ion_count] = func(ld, x, y, z);
+					ion_count++;
+				}
+			}
+		}
+		Latice3D_Data new_lattice(ld.get_Lx(), ld.get_Ly(), ld.get_Lz(), ld.get_cell0().x, ld.get_cell0().y, ld.get_cell0().z);
+		new_lattice.set_cells(new_cells);
+		return new_lattice;
 	}
-};
+}
+namespace Lattice3D_Setters {
+	inline Cell set_cube_cell(const Lattice3D::Latice3D_Data& lattice,int64_t x, int64_t y, int64_t z) {
+		Cell cell_0 = lattice.get_cell0();
+		int64_t x0 = cell_0.x;
+		int64_t y0 = cell_0.y;
+		int64_t z0 = cell_0.z;
+		if ((0>x||x >= lattice.get_Lx()) ||
+			(0>y||y >= lattice.get_Ly() )||
+			(0>z||z >= lattice.get_Lz())) {
+			throw std::out_of_range("Coordinates out of range");
+		}
+		int64_t x_cell = (x0 + x);
+		int64_t y_cell = (y0 + y);
+		int64_t z_cell = (z0 + z);
+		return Cell(x_cell, y_cell, z_cell);
+	}
+}
